2d0285e200497ac02043f63905181d4c03773ed8
damien-cremilleux <damien.cremilleux@insa-rennes.fr>
2013-12-05 12:08:49 +0100

    Mise en place de la fabrique de case

--- VisualStudio/ProjetPOO/Case.cs
+++ VisualStudio/ProjetPOO/Case.cs
@@ -12,4 +12,64 @@ namespace SmallWorld
     public abstract class Case : InterCase
     {
     }
+
+    public interface InterDesert : InterCase
+    {
+    }
+
+    public class Desert : Case, InterDesert
+    {
+        public Desert()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface InterEau : InterCase
+    {
+    }
+
+    public class Eau : Case, InterEau
+    {
+        public Eau()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface InterForet : InterCase
+    {
+    }
+
+    public class Foret : Case, InterForet
+    {
+        public Foret()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface InterMontagne : InterCase
+    {
+    }
+
+    public class Montagne : Case, InterMontagne
+    {
+        public Montagne()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface InterPlaine : InterCase
+    {
+    }
+
+    public class Plaine : Case, InterPlaine
+    {
+        public Plaine()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
