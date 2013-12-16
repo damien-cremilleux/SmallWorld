a918fc6bfb1c8c59e0f0c56e3b26176d6f474c30
Damien Crémilleux <damien.cremilleux@insa-rennes.fr>
2013-12-16 18:37:40 +0100

    Implémentation des unités

--- VisualStudio/ProjetPOO/Unite.cs
+++ VisualStudio/ProjetPOO/Unite.cs
@@ -1,125 +1,249 @@
-﻿using System;
+﻿/**
+ * @file Unite.cs
+ * @brief Interface et classe pour les unités
+ * 
+ * Les unités sont déplacés par les joueurs. Selon le peuple, les unités ont des caractéristiques différentes.
+ * 
+ * @author <a href="mailto:damien.cremilleux@insa-rennes.fr">Damien Crémilleux</a>
+ * @author <a href="mailto:lauriane.holy@insa-rennes.fr">Lauriane Holy</a>
+ * 
+ * @date 26/12/2013
+ * @version 0.1
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace SmallWorld
 {
+    /**
+     * @interface InterUnite
+     * @brief Interface pour les unités
+     */
     public interface InterUnite
     {
+        /**
+         * @fn attaquer
+         * @brief attaquer une case contenant des unités ennemies
+         */
         void attaquer();
 
+        /**
+         * @fn seDeplacer
+         * @brief se déplacer sur une case
+         */
         void seDeplacer();
 
+        /**
+         * @fn passerSonTour
+         * @brief l'unité passe son tour
+         */
         void passerSonTour();
     }
 
+    /**
+     * @class Unite
+     * @brief Classe abstraite pour les unités
+     */
     public abstract class Unite : InterUnite
     {
 
-        public int position
+        /**
+         * @brief Attribut <b>position</b>, contient la position courante de l'unité
+         */
+        private Coordonnees position;
+
+        /**
+         * @brief Attribut <b>pointDeVie</b>, contient le nombre de point de vie restant de l'unité
+         */
+        private int pointDeVie;
+
+        /**
+         * @brief Attribut <b>pointDeDeplacement</b>, contient le nombre de point de déplacement restant de l'unité
+         * 
+         * Le nombre de point de déplacement est réinitialisé à chaque début de tour.
+         */
+        private int pointDeDeplacement;
+
+        /**
+         * @brief Attribut <b>pointDeVictoire</b>, contient le nombre de victoire apporté par de l'unité
+         */
+        private int pointDeVictoire;
+
+        /**
+         * @brief Attribut <b>pointAttaque</b>, contient le nombre de point d'attaque de l'unité
+         */
+        private int pointAttaque;
+
+        /**
+          * @brief Attribut <b>pointDefense</b>, contient le nombre de point de défense de l'unité
+          */
+        private int pointDefense;
+
+        /**
+          * @brief Attribut <b>passeSonTour</b>, indique si l'unité passe son tour
+          */
+        private bool passeSonTour;
+
+
+        /**
+         * @fn Position
+         * @brief Properties pour l'attribut position
+         */
+        public Coordonnees Position
         {
             get
             {
-                throw new System.NotImplementedException();
+                return position;
             }
             set
             {
+                position = value;
             }
         }
 
-        public int pointDeVie
+        /**
+         * @fn PointDeVie
+         * @brief Properties pour l'attribut pointDeVie
+         */
+        public int PointDeVie
         {
             get
             {
-                throw new System.NotImplementedException();
+                return pointDeVie;
             }
             set
             {
+                pointDeVie = value;
             }
         }
 
-        public int pointDeDeplacement
+        /**
+         * @fn PointDeDeplacement
+         * @brief Properties pour l'attribut pointDeDeplacement
+         */
+        public int PointDeDeplacement
         {
             get
             {
-                throw new System.NotImplementedException();
+                return pointDeDeplacement;
             }
             set
             {
+                pointDeDeplacement = value;
             }
         }
 
-        public int pointDeVictoire
+        /**
+         * @fn PointDeVictoire
+         * @brief Properties pour l'attribut pointDeVictoire
+         */
+        public int PointDeVictoire
         {
             get
             {
-                throw new System.NotImplementedException();
+                return pointDeVictoire;
             }
             set
             {
+                pointDeVictoire = value;
             }
         }
 
-        public int pointAttaque
+        /**
+         * @fn PointAttaque
+         * @brief Properties pour l'attribut pointAttaque
+         */
+        public int PointAttaque
         {
             get
             {
-                throw new System.NotImplementedException();
+                return pointAttaque;
             }
             set
             {
+                pointAttaque = value;
             }
         }
 
-        public int pointDefense
+        /**
+         * @fn PointDefense
+         * @brief Properties pour l'attribut pointDefense
+         */
+        public int PointDefense
         {
             get
             {
-                throw new System.NotImplementedException();
+                return pointDefense;
             }
             set
             {
+                pointDefense = value;
             }
         }
 
-        public int passeSonTour
+        /**
+         * @fn PasseSonTour
+         * @brief Properties pour l'attribut passeSonTour
+         */
+        public bool PasseSonTour
         {
             get
             {
-                throw new System.NotImplementedException();
+                return passeSonTour;
             }
             set
             {
+                passeSonTour = value;
             }
         }
-    
+
+        /**
+         * @fn attaquer
+         * @brief attaquer une case contenant des unités ennemies
+         */
         public void attaquer()
         {
             throw new NotImplementedException();
         }
 
+        /**
+         * @fn seDeplacer
+         * @brief se déplacer sur une case
+         */
         public void seDeplacer()
         {
             throw new NotImplementedException();
         }
 
-
+        /**
+         * @fn passerSonTour
+         * @brief l'unité passe son tour
+         */
         public void passerSonTour()
         {
             throw new NotImplementedException();
         }
 
+        /**
+         * @fn perdreVie
+         * @brief l'unité perd une vie
+         */
         public void perdreVie()
         {
             throw new System.NotImplementedException();
         }
 
+        /**
+         * @fn mourir
+         * @brief l'unité meurt
+         */
         public void mourir()
         {
             throw new System.NotImplementedException();
         }
     }
 
-   
+
 }
